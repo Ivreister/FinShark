8243744f88047b74e6c363f54d9ab99718dbac52
Ivreister <reversbor@gmail.com>
2025-10-10 09:51:12 +0300

    Registration added

--- api/Data/ApplicationDbContext.cs
+++ api/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,17 +20,37 @@ namespace api.Data
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(modelBuilder); 
-            
-            modelBuilder.Entity<Stock>().ToTable("Stocks");
-            modelBuilder.Entity<Comment>().ToTable("Comments");
-            
-            modelBuilder.Entity<Stock>()
+            base.OnModelCreating(builder);
+
+            builder.Entity<Stock>().ToTable("Stocks");
+            builder.Entity<Comment>().ToTable("Comments");
+
+            builder.Entity<Stock>()
                 .HasMany(s => s.Comments)
                 .WithOne(c => c.Stock)
                 .HasForeignKey(c => c.StockId);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = "1",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+
+                new IdentityRole
+                {
+                    Id = "2",
+                    Name = "User",
+                    NormalizedName = "USER"
+                },
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
+        
     }
 }
